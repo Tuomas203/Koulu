3cea4650e27a817a47869b85c76e1ab2d3ee31f6
Tuomas203 <tuomas.ruuskaa@gmail.com>
2024-11-29 17:38:09 +0200

    laksdjhgas

--- SpedenSpelit/display.cpp
+++ SpedenSpelit/display.cpp
@@ -1,10 +1,23 @@
 #include "display.h"
 
 
+
+int resetPin = 12;
+int shiftClockPin = 11;
+int latchClockPin = 10;
+int outEnablePin = 9;
+int serialInputPin = 8;
+
 void initializeDisplay(void)
 {
+
 // See requirements for this function from display.h
-pinMode(12, );
+
+pinMode(resetPin, OUTPUT);
+pinMode(shiftClockPin, OUTPUT);
+pinMode(latchClockPin, OUTPUT);
+pinMode(outEnablePin, OUTPUT);
+pinMode(serialInputPin, OUTPUT);
 }
 
 
